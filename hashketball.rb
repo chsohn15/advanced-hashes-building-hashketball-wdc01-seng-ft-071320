6eb46f4276a11b69dab806893a6b88f63d191a8b
Christina Sohn <chsohn15@gmail.com>
2020-06-20 03:33:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -10,7 +10,7 @@ hashketball ={
       :points => 22,
       :rebounds => 12,
       :assists => 12,
-      :steals => 3
+      :steals => 3,
       :blocks => 1, 
       :slam_dunks => 1},
       {:player_name
