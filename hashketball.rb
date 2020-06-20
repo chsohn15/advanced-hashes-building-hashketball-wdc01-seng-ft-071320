6ee0d34b25cceff086cd14ba2ec4e1a57640c995
Christina Sohn <chsohn15@gmail.com>
2020-06-20 03:34:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -12,43 +12,43 @@ hashketball ={
       :assists => 12,
       :steals => 3,
       :blocks => 1, 
-      :slam_dunks => 1,},
-      {:player_name
-      :number
-      :shoe
-      :points
-      :rebounds
-      :assists
-      :steals
-      :blocks
-      :slam_dunks},
-      {:player_name
-      :number
-      :shoe
-      :points
-      :rebounds
-      :assists
-      :steals
-      :blocks
-      :slam_dunks},
-      {:player_name
-      :number
-      :shoe
-      :points
-      :rebounds
-      :assists
-      :steals
-      :blocks
-      :slam_dunks},
-      {:player_name
-      :number
-      :shoe
-      :points
-      :rebounds
-      :assists
-      :steals
-      :blocks
-      :slam_dunks}],
+      :slam_dunks => 1},
+      {:player_name => "Alan Anderson",
+      :number => 0,
+      :shoe => 16,
+      :points => 22,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 3,
+      :blocks => 1, 
+      :slam_dunks => 1},
+      {:player_name => "Alan Anderson",
+      :number => 0,
+      :shoe => 16,
+      :points => 22,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 3,
+      :blocks => 1, 
+      :slam_dunks => 1},
+      {:player_name => "Alan Anderson",
+      :number => 0,
+      :shoe => 16,
+      :points => 22,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 3,
+      :blocks => 1, 
+      :slam_dunks => 1},
+      {:player_name => "Alan Anderson",
+      :number => 0,
+      :shoe => 16,
+      :points => 22,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 3,
+      :blocks => 1, 
+      :slam_dunks => 1}],
   },
     
   
