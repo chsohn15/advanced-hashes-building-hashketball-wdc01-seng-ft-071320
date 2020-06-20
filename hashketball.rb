557bed219ed9f963b90330ca3675869b759c0c88
Christina Sohn <chsohn15@gmail.com>
2020-06-20 03:33:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,6 +4,15 @@ hashketball ={
     :team_name => "TODO",
     :colors=>["TODO"],
     :players =>[
+      {:player_name => "Alan Anderson",
+      :number => 0,
+      :shoe => 16,
+      :points => 22,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 3
+      :blocks => 1, 
+      :slam_dunks => 1},
       {:player_name
       :number
       :shoe
@@ -13,7 +22,33 @@ hashketball ={
       :steals
       :blocks
       :slam_dunks},
-      {}],
+      {:player_name
+      :number
+      :shoe
+      :points
+      :rebounds
+      :assists
+      :steals
+      :blocks
+      :slam_dunks},
+      {:player_name
+      :number
+      :shoe
+      :points
+      :rebounds
+      :assists
+      :steals
+      :blocks
+      :slam_dunks},
+      {:player_name
+      :number
+      :shoe
+      :points
+      :rebounds
+      :assists
+      :steals
+      :blocks
+      :slam_dunks}],
   },
     
   
